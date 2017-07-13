987a2308790c268e5065e9e3792bbede25b49d89
gshaw-pivotal <gshaw+github@pivotal.io>
2017-07-13 14:13:07 -0400

    Add alternative 'qual-text' request
    This is the first step in implementing two variations on how the bot
    gives back air quality information. Currently both 'qual' and
    'qual-text' give identical responses.
    
    In the future 'qual-text' will be a text only response and 'qual' will
    use slack's attachment option to add color indicators.

--- src/controllers/air_qual_controller.js
+++ src/controllers/air_qual_controller.js
@@ -4,6 +4,12 @@ var request =  require('request');
 require('../requesters/air_qual_requester');
 
 setupAirQualController = function (controller) {
+    controller.hears(['qual-text (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
+
+        var location = message.text.match(/qual-text (.*)/i)[1];
+        makeAirQualRequest(controller, bot, message, location)
+    });
+
     controller.hears(['qual (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
 
         var location = message.text.match(/qual (.*)/i)[1];
